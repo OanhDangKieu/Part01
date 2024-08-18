3c034a96f64ab2c0c5ab78671e871114921033da
OanhDangKieu <kieuoanhchtq136@gmail.com>
2024-08-18 13:04:48 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mèo Ú Coffee</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" >
+    <script src="https://kit.fontawesome.com/41e46c9144.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <?php
+    echo '<div class="ab-bg py-5 my-5">
+        <div class="  " id="about">
+            <div class="flex-wrap">
+                <div class=" wrap  ">
+                    <div class="">
+                        <h5 class="a-two mb-3">Giới thiệu</h5>
+                        <h1 class="mt">Mèo Ú Coffee</h1>
+                        <p class="mt-5">Mèo Ú Coffee tự hào là nơi thiên đàng dành riêng cho những người yêu thú cưng, đặc biệt là những người yêu nuôi mèo. Tại đây, những chú mèo chính là ngôi sao sáng giữa không gian ấm cúng và hương vị cà phê tuyệt vời.</p>
+                        <button class="btn "><a class="nav-link" href="">Xem thêm</a></button>
+                    </div>
+                </div>
+                <div class=" flip-right  ">
+                        <img class="img-fluid ms-0 ms-sm-5 shadow rounded image-one" src="banner.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>';
+    ?>  
+</body>
+</html>
